4e7c74f971c37386c91010d9cc1cce42ac3e582d
Akio Kanno <kno.rsr75@gmail.com>
2023-06-14 09:35:25 +0900

    make page directory

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 var server = http.createServer(
     (request,response)=>{
-        fs.readFile('./index.html','UTF-8',(error,data)=>{
+        fs.readFile('./page/index.html','UTF-8',(error,data)=>{
             response.writeHead(200, {'Content-Type':'text/html'});
             response.write(data);
             response.end();
