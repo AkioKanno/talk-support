c918a5f8447b1f9948e57053809685047440d232
Akio Kanno <kno.rsr75@gmail.com>
2023-06-15 11:06:02 +0900

    add body parser

--- api/ConvertVoice.js
+++ api/ConvertVoice.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(cors());
+app.use(express.json())
 
 // レスポンスのデータ
 const responseObjectData = {
@@ -16,7 +18,7 @@ const responseObjectData = {
 app.post("/api/ConvertVoice", function(req, res) {
   res.header('Access-Control-Allow-Methods', 'POST');
 
-  console.log(req);
+  console.log(req.body);
 
   // データの返却
   res.json(responseObjectData);
