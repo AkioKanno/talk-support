53672f4489e3f64532e4784e4a0622c29b35f943
Akio Kanno <kno.rsr75@gmail.com>
2023-06-20 20:13:00 +0900

    Open AIのAPIが返ってくる前の時間稼ぎの表示

--- module/speechRec.js
+++ module/speechRec.js
@@ -11,12 +11,15 @@ recognition.onresult = (event) => {
     // 画面に質問を反映
     this.addChatUI(quetion.toString(), "left")
 
+    var shortQuetion = "「" + quetion + "」の文を短縮して"
+    console.log(shortQuetion)
     // OpenAIに質問を投げる
+    this.makeShortQuetion(shortQuetion)
     this.requestOpenAI(quetion)
 
     // 一番下までスクロール(固定値：1000px分下にスクロール)
     // TODO: チャットの長さ分下スクロールするようにいつか頑張る
-    $('body,html').animate({scrollTop:1000}, 200, 'swing');
+    $('body,html').animate({scrollTop:10000}, 200, 'swing');
 }
 
 $(function() {
@@ -24,6 +27,53 @@ $(function() {
         recognition.start()
     })
 })
+/**
+ * OpenAI API に質問をする
+ *
+ * @param quetion : 質問内容
+ */
+function makeShortQuetion(quetion) {
+    xhr = new XMLHttpRequest;
+    var qCount = Number($("#quetionCount").val())
+    var reqJson = {
+        "quetion"        : quetion,
+        "quetionCount"   : qCount + 1,
+        "emotionalValue" : $("#emotionalValue").val()
+    }
+
+
+    // Request 生成
+    xhr.open('post', "http://localhost:3001/api/responceAI", true);
+    xhr.setRequestHeader('Content-Type', 'application/json;charset=utf8');
+    xhr.send(JSON.stringify(reqJson));
+    // Responce の処理
+    xhr.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            // 発言を設
+            const uttr = new SpeechSynthesisUtterance()
+            console.log("---- Short Quetion -----")
+            console.log(this.responseText)
+            var resJson = JSON.parse(this.responseText)
+            var answer = "質問ありがとうございます。" + resJson.answer + " についての質問ですね。"
+            var emotionalValue = resJson.emotionalValue
+            console.log(answer)
+            // Speech Text設定
+            uttr.text = answer
+            uttr.lang = "ja-JP"
+            // 速度を設定
+            uttr.rate = 0.8
+            // 高さを設定
+            uttr.pitch = 1.1
+            // 音量を設定
+            uttr.volume = 1.3
+            // 発言を再生 (必須)
+            window.speechSynthesis.speak(uttr)
+            addChatUI(answer, "right")
+            // hidden Parameterに設置
+            document.getElementById("emotionalValue").value = resJson.emotionalValue
+        }
+    }
+}
 
 /**
  * OpenAI API に質問をする
@@ -51,7 +101,7 @@ function requestOpenAI(quetion) {
             const uttr = new SpeechSynthesisUtterance()
             console.log(this.responseText)
             var resJson = JSON.parse(this.responseText)
-            var answer = resJson.answer
+            var answer = resJson.answer.replace(/^ください/g,"")
             var emotionalValue = resJson.emotionalValue
             console.log(answer)
             // Speech Text設定
